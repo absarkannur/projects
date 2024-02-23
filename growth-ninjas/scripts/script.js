03952996f971a72770af1be6095d85adcb8d23e1
Muhammed Absar <121879746+absarkannur@users.noreply.github.com>
2024-02-24 03:01:42 +0400

    some new updates

--- growth-ninjas/scripts/script.js
+++ growth-ninjas/scripts/script.js
@@ -1,6 +1,25 @@
 // : )
+
 $(function(){
 
-    // Hello
+    $(document).ready(function(){
+        var scroll = $(window).scrollTop();
+        if( scroll >= 1300 ) {
+            $('.banner-fixed').show();
+        } else {
+            $('.banner-fixed').hide();
+        }
+    });
+    $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+        if( scroll >= 1300 ) {
+            $('.banner-fixed').show();
+        } else {
+            $('.banner-fixed').hide();
+        }
+    });
+
+    // -------
+
 
 });
